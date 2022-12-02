9280007737342db0d64a088d62d7b0e0655b1aa3
Messan Ayayi <83885361+cinopepito@users.noreply.github.com>
2022-12-02 19:00:35 +0100

    Add innitial cv website files

---
+++ index.js
@@ -0,0 +1,34 @@
+// var name = "jack";
+// var age = 23;
+// var message = "hi, my name is " + name + " i'm " + age + "years old.";
+// console.log(message);
+
+// var firstName = "john";
+// var lastName = "jacobJingHeimerSmith";
+// var dateOfBirth = "10-09-82";
+// var age = 23;
+// var profileImg = "https://coolpics.com/jacobPic.jpg";
+
+// var loginWelcomeMessage =
+//   "welcome " + firstName + " Happy " + age + "rd birthday";
+// console.log(loginWelcomeMessage);
+// var sum = 10 + 15;
+// console.log(sum);
+// var mod = 10 % 3;
+
+// var message = "10/3 = 3 with a remainder of " + mod;
+// console.log(message);
+// var myAcountBalance = 300;
+// var nikeSbShoes = 799.23;
+// var coupon = 500;
+// if (nikeSbShoes <= myAcountBalance) {
+//   myAcountBalance = myAcountBalance - nikeSbShoes;
+//   console.log("we just bought some dope shoes");
+//   console.log("acount balance:" + myAcountBalance);
+// } else if (nikeSbShoes - coupon <= myAcountBalance) {
+//   console.log("we just bought some dope shoes with a coupon !");
+//   myAcountBalance = nikeSbShoes - coupon;
+//   console.log("acount balance:" + myAcountBalance);
+// } else {
+//   console.log("you too broke for this shoes bro !");
+// }
